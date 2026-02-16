6cf59ceb4042ac041d4266987370000472a41483
Rajat <chhabrarajat850@gmail.com>
2026-02-17 03:12:35 +0530

    Initial commit: Add C learning project files

---
+++ rajat.c
@@ -0,0 +1,249 @@
+// #include <stdio.h>
+
+// int main(){
+//     printf("Hey zephy boi on top!!");
+//     printf("My name is rajat chhabra");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("Hey how are u");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("Hey how are u man");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("My name is rajat chhabra!!!");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("Zephy boiii on top!!");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("Rajat");
+//     printf("Chhabra");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("Zephyy boiii mat jaooo vc se bar bar!!");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("Aakash");
+//     printf(" ");
+//     printf("chatterjee");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("Rajat");
+//     printf(" ");
+//     printf("Chhabra");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("Worklikehell");
+//     printf(" -- ");
+//     printf("Rajat");
+//     printf(" ");
+//     printf("Chhabra");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("Rajat--->>>>");
+//     printf(" ");
+//     printf("<<<<<------");
+//     printf("Chhabra");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("...........>>>>>>");
+//     printf("               ");
+//     printf("<<<<<<---------");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("Think always big.......>>>>>>");
+//     printf("         ");
+//     printf("<<<<<<------Umm!!!");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("<<<------>>>>");
+//     printf("    ");
+//     printf("<<<<------>>>>")
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("Worklikehell-------->>>>>");
+//     printf("  ");
+//     printf("Rajat");
+//     printf(" ");
+//     printf("Chhabra!!!");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("<--------->");
+//     printf(" ");
+//     printf("<--------->");
+//     printf(" ");
+//     printf("<-------->");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("<<<<------->>>>");
+//     printf(" ");
+//     printf("worklikehell");
+//     printf(" ");
+//     printf("Rajat chhabra");
+//     printf(" ");
+//     printf("<<<<<--------->>>>>");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("<<<<<------->>>>>");
+//     printf(" ");
+//     printf("Worklikehell!!!");
+//     printf(" ");
+//     printf("Worklikehell");
+//     printf(" ");
+//     printf("Rajatchhabra");
+//     printf(" ");
+//     printf("<<<<---------->>>>>");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("Hello World");
+//     printf(" ");
+//     printf("<<<<-------->>>>>");
+//     printf("My name is rajat chhabra");
+//     printf(" ");
+//     printf("<<<<-------->>>>");
+//     printf("Work hard until u win");
+//     printf(" ");
+//     printf("<<<<--------->>>>>>");
+//     printf("Hey how are u man i love u so much");
+//     printf(" ");
+//     printf("<<<<<<----------->>>>>>");
+//     printf("I started my own company");
+//     printf(" ");
+//     printf("<<<<<<---------->>>>>>>");
+//     printf("Think always big and work hard");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("<<<<<<--------------------->>>>>>");
+//     printf("<<<<<<<<------------------->>>>>>>");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("rajat");
+//     printf(" ");
+//     printf("Worklikehell");
+//     printf(" ");
+//     printf(" ");
+//     printf("Umm!!");
+//     return 0;
+// }
+
+// #include<stdio.h>
+
+// int main(){
+//     printf("Rajat");
+//     printf(" ");
+//     printf("Chhabra");
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("Rajat");
+//     printf(" ");
+//     printf("Chhabra");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main() {
+//     printf("Ruddii");
+//     printf(" ");
+//     printf("Boiii");
+//     printf(" ");
+//     printf("On top");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     printf(" ");
+//     printf("<<<<<---------->>>>>>");
+//     printf("    ");
+//     printf("Worklikehell");
+//     printf("    ");
+//     printf("Think always big");
+//     return 0;
+// }
